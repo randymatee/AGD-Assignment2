a07d1ad3a6d2cb0fb49a931150ad8b9e70dbad56
randymater <164705552+randymater@users.noreply.github.com>
2026-05-12 15:44:47 +0930

    Initial commit

---
+++ assets/AllInOne.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="AllInOne" tilewidth="32" tileheight="32" tilecount="52" columns="4">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="../../Downloads/4 BigSet.png" width="128" height="432"/>
+</tileset>
---
+++ assets/Floor.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Floor" tilewidth="32" tileheight="32" tilecount="8" columns="4">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="../../Downloads/4 Walls, Floor &amp; Doors.png" width="128" height="80"/>
+</tileset>
