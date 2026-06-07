5f4492083d9d18792b4b829647fc98a7733e75a3
HaydenClarkM <a1884737@adelaide.edu.au>
2026-06-07 13:56:57 +0930

    Fix player positon.

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -43,7 +43,7 @@ public class Player extends Entity implements ApplicationListener {
         setTextureRows(1);
         setTextureCols(1);
         setAnimationFrames(new TextureRegion[1 * 1]);
-        setStartingPositon(new Vector2(1600, 100));
+        setStartingPositon(new Vector2(50, 100));
 
 
         parseSprite(getTextureSheet(), getAnimationFrames(), 1, 1);
